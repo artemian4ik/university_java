8084d6040ffd96db37cda9bec0c4cd2e722ba6f1
artemian4ik <nyashev228@gmail.com>
2020-11-04 19:22:49 +0300

    laba2

---
+++ laba2/2_2/MyApp/src/acrostic/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acrostic;
+/**
+ *
+ * @author nonst
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String result = org.me.mylib.LibClass.acrostic(args);
+        System.out.println("Result = " + result);
+    }
+}
